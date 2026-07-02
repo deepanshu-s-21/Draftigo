451b17006660d204f5263d2f68d0806a23d2c669
Deepanshu Singh <deepanshu124singh@gmail.com>
2026-07-02 17:15:11 +0530

    Refactor repository layout and update project implementation

---
+++ Backend/src/main/java/com/email/draftigo/DraftigoApplication.java
@@ -0,0 +1,11 @@
+package com.email.draftigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftigoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DraftigoApplication.class, args);
+	}
+}
---
+++ Backend/src/test/java/com/email/draftigo/DraftigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.draftigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
